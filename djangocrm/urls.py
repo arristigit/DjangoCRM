cea5d9bf3da2ba8fa2277568ffc780ffa4296c34
arristigit <arristimedia@gmail.com>
2021-09-15 00:45:45 +0530

    Reset password added

--- djangocrm/urls.py
+++ djangocrm/urls.py
@@ -19,17 +19,25 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordResetView, PasswordResetDoneView, 
+    PasswordResetConfirmView, PasswordResetCompleteView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="admin"),
+    path('leads/', include('leads.urls', namespace="leads")),
+    path('agents/', include('agents.urls', namespace="agents")),
+    
     path('', LandingPageView.as_view(), name="landing-page"),
     path('signup/', SignUpView.as_view(), name="signup"),
     path('login/', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
+    path('reset-password/', PasswordResetView.as_view(), name="reset-password"),
+    path('password-reset-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     # path('', landing_page, name="landing-page"),
-    path('leads/', include('leads.urls', namespace="leads")),
-    path('agents/', include('agents.urls', namespace="agents")),
 ]
 
 if settings.DEBUG:
